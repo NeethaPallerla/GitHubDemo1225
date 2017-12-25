baf1786d06db19e461dfe0c224ed7aebf5957654
nspal <nspallerla@gmail.com>
2017-12-25 12:06:40 -0500

    Change-Id: I695a8258c3c191df82b44d87997487ef96c06b48 Signed-off-by: nspal <nspallerla@gmail.com>

--- GitHubPractice/src/GitHubPracticePackage/GitHubpracticeClass.java
+++ GitHubPractice/src/GitHubPracticePackage/GitHubpracticeClass.java
@@ -5,6 +5,7 @@ public class GitHubpracticeClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
  System.out.println("Testing GitHub");
+ System.out.println("First commit Test");
 	}
 
 }
