5738f88889fc018562efae91315b10958d835737
nspal <nspallerla@gmail.com>
2017-12-25 11:28:41 -0500

    Change-Id: Ia56134162a8368f684dc78198de0aad61aa8bb06

---
+++ GitHubPractice/src/GitHubPracticePackage/GitHubpracticeClass.java
@@ -0,0 +1,10 @@
+package GitHubPracticePackage;
+
+public class GitHubpracticeClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("Testing GitHub");
+	}
+
+}
